d0e93fff1d895fbba0bfcbfbcb9bf586645ba137
sunnyliao88 <sunnyliao88@gmail.com>
2020-01-11 11:21:02 -0500

    add css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css'
+
+// function Footer(){
+//     return (
+//         <footer className="text-center test">
+//             <p>Copy right Sunny2020</p>
+//         </footer>
+//     );
+// }
+
+const Footer = () => {
+    const myname="Sunny LIAO";
+    const year=2020;
+    return (
+        <footer className="text-center test">
+            <p>Copy right {myname} {year}</p>
+        </footer>
+    )
+}
+
+
+export default Footer;
---
+++ src/components/employee.component.jsx
@@ -0,0 +1,9 @@
+import Component from 'react';
+
+class EmployeeComponent extends Component {
+
+    render(
+        return (<h1>test</h1>)   );
+}
+
+export default EmployeeComponent;
